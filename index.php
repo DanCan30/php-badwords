9cb64c805e9b75743b4bd761efbed76631e24083
DanCan30 <danielecan3008@gmail.com>
2022-08-22 16:05:18 +0200

    Badwords filter added

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php 
 
     $text = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, amet! Doloremque dolores tenetur illum sint praesentium modi quasi molestiae ullam. Sint, esse pariatur vero cupiditate nihil est provident eaque rerum?";
+    $badWord = $_GET["filter"];
+    $censoredText = str_ireplace($badWord, "***", $text);
 
 ?>
 
@@ -13,13 +15,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BADWORDS</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>PHP Badwords</h1>
     <p>
-        <?php 
+        <?php
             echo $text;
         ?>
+        <span>
+
+            <?php
+                echo strlen($text);
+            ?>
+        </span>
     </p>
+    
+    <p>
+        <?php
+            echo $censoredText;
+        ?>
+        
+        <span>
+
+            <?php
+                echo strlen($censoredText);
+            ?>
+        </span>
+    </p>
+
 </body>
 </html>
