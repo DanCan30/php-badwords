15abd8b8fa9558d967c04f0b30e1ce032e33dca5
DanCan30 <danielecan3008@gmail.com>
2022-08-22 15:23:30 +0200

    Base structure done

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+    $text = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, amet! Doloremque dolores tenetur illum sint praesentium modi quasi molestiae ullam. Sint, esse pariatur vero cupiditate nihil est provident eaque rerum?";
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BADWORDS</title>
+</head>
+<body>
+    <h1>PHP Badwords</h1>
+    <p>
+        <?php 
+            echo $text;
+        ?>
+    </p>
+</body>
+</html>
